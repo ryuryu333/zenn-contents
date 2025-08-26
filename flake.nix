8313321ae9513410d7d6cce74423bf46ad45ee04
ryuryu333 <risarisa062518@gmail.com>
2025-08-26 22:37:53 +0900

    flake.nix に textlint パッケージを追加

--- flake.nix
+++ flake.nix
@@ -12,10 +12,19 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        my-node-pkgs = import ./node-pkgs/default.nix {
+          inherit pkgs system;
+          nodejs = pkgs.nodejs_24;
+          };
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = [ pkgs.zenn-cli ];
+          packages = [
+            pkgs.zenn-cli
+            my-node-pkgs."textlint-15.2.2"
+            my-node-pkgs."textlint-rule-preset-ja-spacing-2.4.3"
+            my-node-pkgs."textlint-rule-preset-ja-technical-writing-12.0.2"
+            ];
         };
       }
     );
